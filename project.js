c3572ef2ada783efb264546ffc9a2463c27679ba
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-11-03 12:54:37 -0600

    Corrected responsiveness

--- project.js
+++ project.js
@@ -1,15 +1,15 @@
 let projectsArray = [
     {
         title: 'Vehicle Booking App',
-        description: "An App where people can rent or book any vintage vehicle of their choice for the number of days they want. Utilizes React-Redux and Ruby on Rails.",
-        languages: ['html', 'Bootstrap', 'Ruby', 'React', 'Crypto-js', 'Axios'],
+        description: "An App where people can rent or book any vintage vehicle of their choice for a period of time. It Utilizes React-Redux and Ruby on Rails.",
+        languages: ['Bootstrap', 'Ruby', 'React', 'Rails'],
         livelink: 'https://bookit-fe.netlify.app/',
         sourcelink: 'https://github.com/Adanzeakonobi/bookit-fe.git',
         image:"Images/bookit.png",
       },
     {
       title: 'Recipe App',
-      description: "This keeps track of all your recipes and ingredients. It allows you to create recipes and generate shopping list based on what you have and what you are missing from a recipe.",
+      description: "This keeps track of all your recipes and ingredients. It allows you to create recipes and generate shopping list from the recipe.",
       languages: ['Rails', 'Bootstrap', 'Ruby'],
       livelink: 'https://aj-recipeapp-staging.herokuapp.com/',
       sourcelink: 'https://github.com/Adanzeakonobi/Recipe-App',
@@ -17,7 +17,7 @@ let projectsArray = [
     },
     {
       title: 'Budget App',
-      description: "This is a mobile web application where you can manage your budget, you have a list of transactions associated with categories, to enable you see amount spent.",
+      description: "This is a mobile web application where you can manage your budget, you have a list of transactions associated with categories.",
       languages: ['Rails', 'Bootstrap', 'Ruby '],
       livelink: 'https://personal-budgetlist.herokuapp.com/',
       sourcelink: 'https://github.com/Adanzeakonobi/Budget_App.git',
@@ -25,7 +25,7 @@ let projectsArray = [
     },
     {
       title: 'Space Travel',
-      description: "This App is for a company that provides commercial and scientific space travel services. The website allow users to book rockets and join selected space missions.",
+      description: "This Webapp provides commercial space travel services where users are allowed to book rockets and join selected space missions.",
       languages: ['HTML', 'CSS', 'React', 'Redux'],
       livelink: 'https://space-travelers-hubby.netlify.app/',
       sourcelink: 'https://github.com/Adanzeakonobi/Space--Travelers-App.git',
@@ -41,7 +41,7 @@ let projectsArray = [
     },
     {
       title: 'Conference Page',
-      description: "This is a mobile and web app designed for a planned graduate trainee program. I created to pages for home page and about page. ",
+      description: "This is a mobile and web app designed for a planned graduate trainee program. I created to pages for home page and about page.",
       languages: ['HTML', 'CSS', 'JavaScript'],
       livelink: 'https://adanzeakonobi.github.io/Conference-Page/',
       sourcelink: 'https://github.com/Adanzeakonobi/Conference-Page.git',
